510f0333f0d8d2680d864924aa6d1945223ef313
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2022-04-25 21:34:09 +0600

    Adjacent movement

--- checker/board.py
+++ checker/board.py
@@ -60,56 +60,50 @@ class Board:
     def get_valid_moves(self, piece: Piece):
         valid_moves: list[tuple[int, int]] = []
 
-        if piece.get_king():
-            top_left = self._traverse_left(
-                piece.get_row() - 1, piece.get_col() - 1, piece.get_color())
-            top_right = self._traverse_right(
-                piece.get_row() - 1, piece.get_col() + 1, piece.get_color())
-            bottom_left = self._traverse_left(
-                piece.get_row() + 1, piece.get_col() - 1, piece.get_color())
-            bottom_right = self._traverse_right(
-                piece.get_row() + 1, piece.get_col() + 1, piece.get_color())
-
-            valid_moves.extend(top_left)
-            valid_moves.extend(top_right)
-            valid_moves.extend(bottom_left)
-            valid_moves.extend(bottom_right)
-
         if piece.get_color() == "white":
             direction = 1
-            print(piece.get_color())
-
-            left = self._traverse_left(
-                piece.get_row() + direction,
-                piece.get_col() - direction,
-                piece.get_color(),
-            )
-            right = self._traverse_right(
-                piece.get_row() + direction,
-                piece.get_col() + direction,
-                piece.get_color(),
-            )
-
-            valid_moves.extend(left)
-            valid_moves.extend(right)
+            moves = self.adjacent_move(piece, direction)
+            valid_moves.extend(moves)
         else:
             direction = -1
+            moves = self.adjacent_move(piece, direction)
+            valid_moves.extend(moves)
 
-            left = self._traverse_left(
-                piece.get_row() + direction,
-                piece.get_col() + direction,
-                piece.get_color(),
-            )
-            right = self._traverse_right(
-                piece.get_row() + direction,
-                piece.get_col() - direction,
-                piece.get_color(),
-            )
+        if piece.get_king() and piece.get_color() == "white":
+            top = self.adjacent_move(piece, direction=-1)
 
-            valid_moves.extend(left)
-            valid_moves.extend(right)
+            valid_moves.extend(top)
+        if piece.get_king() and piece.get_color() == "black":
+            bottom = self.adjacent_move(piece, direction=1)
+            valid_moves.extend(bottom)
 
         return valid_moves
+
+    def adjacent_move(self, piece: Piece, direction: int):
+        valid: list[tuple[int, int]] = []
+        row, col = piece.get_row(), piece.get_col()
+
+        if (direction == 1 and row == 7) or (direction == -1 and row == 0):
+            return valid
+
+        if col == 0:
+            if self._check_adjacent(row + direction, col + 1) is not None:
+                valid.append((row + direction, col + 1))
+        elif col == 7:
+            if self._check_adjacent(row + direction, col - 1) is not None:
+                valid.append((row + direction, col - 1))
+        else:
+            if self._check_adjacent(row + direction, col + 1) is not None:
+                valid.append((row + direction, col + 1))
+            if self._check_adjacent(row + direction, col - 1) is not None:
+                valid.append((row + direction, col - 1))
+
+        return valid
+
+    def _check_adjacent(self, row: int, col: int):
+        if self.board[row][col] is None:
+            return row, col
+        return None
 
     def _traverse_left(self, row: int, col: int, color: ColorType):
         valid: list[tuple[int, int]] = []
@@ -169,15 +163,13 @@ class Board:
 
         piece.move(row, col)
 
-        if row == 0 or row == ROW - 1:
+        if (row == 0 or row == ROW - 1) and not piece.get_king():
             piece.make_king()
 
-        if piece.get_color() == "white":
-            self.white_kings += 1 if piece.get_king() else 0
-            self.white_left -= 1
-        else:
-            self.black_kings += 1 if piece.get_king() else 0
-            self.black_left -= 1
+            if piece.get_color() == "white":
+                self.white_kings += 1 if piece.get_king() else 0
+            else:
+                self.black_kings += 1 if piece.get_king() else 0
 
     def check_winner(self):
         if self.black_left == 0:
